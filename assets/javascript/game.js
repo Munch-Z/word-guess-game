cc8ee7cddc8de1f5c4d26dbba827c488af4f983d
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-05-31 15:44:44 -0700

    Placeholder and on key events working, incorrect letter bank still shows keycode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,38 @@
-var playNowButtonHTML = document.getElementById("playNow");
-var blankWordsHTML = document.getElementById("currentWordInput");
-var lettersGuessedHTML = document.getElementById("lettersGuessedInput");
-var guessesRemainingHTML = document.getElementById("guessesRemainingInput");
-var winsHTML = document.getElementById("winsInput");
-var lossesHTML = document.getElementById("lossesInput");
-var perksCheckHTML = document.getElementById("perksCheck");
-var equipmentCheckHTML = document.getElementById("equipmentCheck");
+//Interaction with the DOM
 
-var wordBanks = {
-    Perks: ["Marathon", "Sleight of Hand", "Scavenger", "Bling", "One Man Army", "Stopping Power", "Lightweight", "Hardline", "Cold Blooded", "Danger Close", "Commando", "Steady Aim", "Scrambler", "Ninja", "SitRep", "Last Stand"],
-    Equipment: ["Frag", "Semtex", "Throwing Knife", "Tactical Insertion", "Blast Sheild", "Claymore", "C4", "Stun Grenade", "Flash Grenade", "Smoke Grenade"]
+let playNowButtonHTML = document.getElementById("playNow");
+let blankWordsHTML = document.getElementById("currentWordInput");
+let lettersGuessedHTML = document.getElementById("lettersGuessedInput");
+let guessesRemainingHTML = document.getElementById("guessesRemainingInput");
+let winsHTML = document.getElementById("winsInput");
+let lossesHTML = document.getElementById("lossesInput");
+let perksCheckHTML = document.getElementById("perksCheck");
+let equipmentCheckHTML = document.getElementById("equipmentCheck");
+let incorrectLettersHTML = document.getElementById("incorrectLettersInput");
+
+//Words/Arrays
+let wordBanks = {
+    Perks: ["marathon", "sleight of hand", "scavenger", "bling", "one man army", "stopping power", "lightweight", "hardline", "cold blooded", "danger close", "commando", "steady aim", "scrambler", "ninja", "sitrep", "last stand"],
+    Equipment: ["frag", "semtex", "throwing knife", "tactical insertion", "blast sheild", "claymore", "stun grenade", "flash grenade", "smoke grenade"]
 };
 
-var randomNumber;
-var pickedWord = [];
-var pickedWordPlaceholder = [];
-var incorrectLetterBank = [];
-var guessedLetterBank = [];
+let randomNumber;
+let pickedWord = [];
+let pickedWordPlaceholder = [];
+let incorrectLetterBank = [];
+let guessedLetterBank = [];
 
-var wins = 0;
-var losses = 0;
-var guessesLeft = 12;
+let keyPressed = KeyboardEvent.key;
+
+//game stats
+let wins = 0;
+let losses = 0;
+let guessesLeft = 12;
 
 //game starts off
-var gameRunning = false;
+let gameRunning = false;
 
-//count is specifically for the limitCheckbox function
 
-var count;
 
 //selects a random word from one of the two arrays
 function getWords(prop) {
@@ -41,10 +46,10 @@ function getWords(prop) {
 function newGame(){
     
     gameRunning = true;
-    guessesLeft;
-    guessedLetterBank;
-    incorrectLetterBank;
-    pickedWordPlaceholder;
+    guessesLeft = 12;
+    guessedLetterBank = [];
+    incorrectLetterBank = [];
+    pickedWordPlaceholder = [];
 
 if (document.getElementById("equipmentCheck").checked) {
         pickedWord = getWords("Equipment");
@@ -66,17 +71,14 @@ if (document.getElementById("equipmentCheck").checked) {
         lettersGuessedHTML.textContent = guessedLetterBank;
 };
 
-playNowButtonHTML.addEventListener("click", newGame);
+playNowButtonHTML.addEventListener("click", () => (gameRunning == true) ? alert("Please guess the word, or run out of guesses to get a new word.") : newGame());
 
 // Only allows one checkbox to be clicked
 
 function limitCheckbox(id, max, checkId) {
-    console.log("I got here 1")
-    var count = 0,
-        i;
-    var checkboxes = document.getElementById(id);
-    for (i = 0; i < checkboxes.length; i += 1) {
-        console.log("I got here 2")
+    let count = 0;
+    let checkboxes = document.getElementById(id);
+    for (let i = 0; i < checkboxes.length; i++) {
         if (checkboxes[i].checked) {
             count++;
 		}
@@ -91,6 +93,38 @@ function limitCheckbox(id, max, checkId) {
 perksCheckHTML.addEventListener("click", () => limitCheckbox("wordBanksHTML", 1, "equipmentCheck"));
 equipmentCheckHTML.addEventListener("click", () => limitCheckbox("wordBanksHTML", 1, "perksCheck"));
 
+function userGuessed (letter) {
+
+   //checks if game is running, and if letter has been guessed 
+   if (gameRunning === true && guessedLetterBank.indexOf(letter) === -1) {
+        guessedLetterBank.push(letter);
+
+            //converts letter to lowercase and pushes if it matches any part of pickedWord
+        for (let i=0; i < pickedWord.length; i++) {
+           if (pickedWord[i] === letter.toLowerCase()) {
+               pickedWordPlaceholder[i] = letter;
+               blankWordsHTML.textContent = pickedWordPlaceholder.join(" ");
+           } else if (pickedWord[i] !== letter.toLowerCase()) {
+            incorrectLettersHTML.textContent =  incorrectLetterBank.push(letter);
+           } else {
+               return;
+           }
+        }
+   }  else if (gameRunning === false) {
+    alert("Click Play Now to start a new game.")
+} else {
+    return;
+}
+}
 
 
+// onkeyup event triggers userGuessed
 
+document.onkeydown = function (event) {
+
+    if (event.keyCode > 64 && event.keyCode < 91) {
+        userGuessed(event.key);
+    } else {
+        
+    }
+}
