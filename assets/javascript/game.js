272ccd04bf9bb51a8f2d92830a114c0356859e04
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-05-30 18:30:55 -0700

    Fixed JS to print spaces instead of _'s when there's a space in the string

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,13 +4,14 @@ var lettersGuessedHTML = document.getElementById("lettersGuessedInput");
 var guessesRemainingHTML = document.getElementById("guessesRemainingInput");
 var winsHTML = document.getElementById("winsInput");
 var lossesHTML = document.getElementById("lossesInput");
-var perkButtonHTML = document.getElementById("perkCheck");
-var equipmentButtonHTML = document.getElementById("equipmentCheck");
+var perksCheckHTML = document.getElementById("perksCheck");
+var equipmentCheckHTML = document.getElementById("equipmentCheck");
 
 var wordBanks = {
     Perks: ["Marathon", "Sleight of Hand", "Scavenger", "Bling", "One Man Army", "Stopping Power", "Lightweight", "Hardline", "Cold Blooded", "Danger Close", "Commando", "Steady Aim", "Scrambler", "Ninja", "SitRep", "Last Stand"],
     Equipment: ["Frag", "Semtex", "Throwing Knife", "Tactical Insertion", "Blast Sheild", "Claymore", "C4", "Stun Grenade", "Flash Grenade", "Smoke Grenade"]
 };
+
 var randomNumber;
 var pickedWord = [];
 var pickedWordPlaceholder = [];
@@ -21,7 +22,12 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 12;
 
+//game starts off
 var gameRunning = false;
+
+//count is specifically for the limitCheckbox function
+
+var count;
 
 //selects a random word from one of the two arrays
 function getWords(prop) {
@@ -48,8 +54,12 @@ if (document.getElementById("equipmentCheck").checked) {
         
         //create placeholder underscores for new word, excluding spaces
         for (let i=0; i < pickedWord.length; i++) {
+            if (pickedWord[i] === ' ') {
+                pickedWordPlaceholder.push("\xa0\xa0\xa0");
+            } else {
             pickedWordPlaceholder.push("_");
         }
+    }
 
         guessesRemainingHTML.textContent = guessesLeft;
         blankWordsHTML.textContent = pickedWordPlaceholder.join(" ");
@@ -74,10 +84,10 @@ function limitCheckbox(id, max, checkId) {
         document.getElementById(checkId).checked = false;
         
     }
-}
+};
 
-perkButtonHTML.onclick = limitCheckbox('wordBanksHTML', 1, 'equipmentCheck');
-equipmentButtonHTML.onclick = limitCheckbox('wordBanksHTML', 1, 'perksCheck');
+perksCheckHTML.onclick = limitCheckbox("wordBanksHTML", 1, "equipmentCheck");
+equipmentCheckHTML.onclick = limitCheckbox("wordBanksHTML", 1, "perksCheck");
 
 
 
